1c4b2ae5053028c904b96f2a7ad074d509ec4c46
Ivy238030@outlook.com <Ivy238030@outlook.com>
2020-05-29 13:38:07 +0300

    My first commit

---
+++ TestWPFAppForHotels/TestWPFAppForHotels/Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPFAppForHotels
+{
+    class Hotel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CountOfStars { get; set; }
+
+        public string CountryName { get; set; }
+
+    }
+  
+}
